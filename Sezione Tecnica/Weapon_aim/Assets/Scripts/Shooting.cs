fd0893aa95bbd552c95ac9243e1f5d6a356e9be1
Andrea Belloni <abelloni2022@gmail.com>
2020-03-12 19:08:07 +0100

    Update Unity 2019.3.5f1

--- Sezione Tecnica/Weapon_aim/Assets/Scripts/Shooting.cs
+++ Sezione Tecnica/Weapon_aim/Assets/Scripts/Shooting.cs
@@ -13,7 +13,7 @@ public class Shooting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Fire1"))
+        if(Input.GetKeyDown(KeyCode.Mouse0))
         {
             Shoot();
         }
