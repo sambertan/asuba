8c2f1d163ac1aa5fdf5b7df9f09a16527ec95b5a
Andrea Belloni <abelloni2022@gmail.com>
2020-03-24 15:57:12 +0100

    Inizio realizzazione scena 2D

---
+++ Sezione Tecnica/Weapon_aim/Assets/Scripts/2D/Movement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Movement : MonoBehaviour
+{
+    public float defaultMovementSpeed = 5f;  
+    public float runMultipler = 2;
+    public Rigidbody2D rb; 
+    public Camera cam;
+    Vector2 movement;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement_character();
+
+    }
+    void Movement_character()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            defaultMovementSpeed *= runMultipler;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+        }
+        movement.x = Input.GetAxisRaw("Horizontal");
+    }
+}
