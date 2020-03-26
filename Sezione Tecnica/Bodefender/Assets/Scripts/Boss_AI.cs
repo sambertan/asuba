8441a577f87402ca903150e0ddc7aafcacb6a07f
Andrea Belloni <abelloni2022@gmail.com>
2020-03-26 19:08:23 +0100

    Rinomino cartella(Bodefender)

---
+++ Sezione Tecnica/Bodefender/Assets/Scripts/Boss_AI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Boss_AI : Enemy
+{
+    //health in mother-class
+
+    public int damage;
+    private float timeBtwDamage = 1.5f;
+
+    public Animator redPanel;
+    public Animator camAnim;
+    public Slider healthBar;
+
+    void Update()
+    {
+        if(timeBtwDamage > 0)
+        {
+            timeBtwDamage -= Time.deltaTime;
+        }
+    }
+}
