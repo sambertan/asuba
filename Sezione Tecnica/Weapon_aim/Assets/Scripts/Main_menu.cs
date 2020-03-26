4a6224447a6e879b501718d7a53644b67278c99a
Andrea Belloni <abelloni2022@gmail.com>
2020-03-26 18:54:25 +0100

    Revert "Menù e Opzioni"
    This reverts commit efbb5798f457facbf88c2d0df1d5be4c6313ae90.

--- Sezione Tecnica/Weapon_aim/Assets/Scripts/Main_menu.cs
+++ Sezione Tecnica/Weapon_aim/Assets/Scripts/Main_menu.cs
@@ -5,13 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class Main_menu : MonoBehaviour
 {
-    public void Play()
+    public Main_menu main_menù;
+   public void Play()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-
     public void Quit()
     {
+        Debug.Log("Arrivesos");
         Application.Quit();
+    }
+    public void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+           
+        }
     }
 }
