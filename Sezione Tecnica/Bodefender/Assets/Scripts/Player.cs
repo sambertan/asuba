ebd42669c4eafa6fb751fd1f1353f5121ef36554
cannibutthenottakenone <48643174+cannibutthenottakenone@users.noreply.github.com>
2020-04-12 18:18:49 +0200

    Implementazione di una superclasse Player e Modifica della scena 2d
    è stata implementata una classe player con le caratteristiche comuni ad entrambi i cntroller del player.
    
    è stata cambiata la griglia della scena 2d in due griglie una esagonale (sfondo) e una quadrata (area di gioco)

---
+++ Sezione Tecnica/Bodefender/Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int maxHealth = 10;
+    public int maxShield = 10;
+    protected int actualHealth;
+    protected int actualShield;
+
+    public int ActualHealth { get => actualHealth; }
+    public int ActualShield { get => actualShield; }
+
+    private void Start()
+    {
+        actualHealth = maxHealth;
+        actualShield = maxShield;
+    }
+
+
+    public void Damage(int amount)
+    {
+        if (amount < 1) return;
+        else if (actualShield > 0)
+        {
+            int oldShield = actualShield;
+            actualShield = Mathf.Clamp(actualShield - amount, 0, maxShield);
+            if(actualShield==0)
+            Damage(Mathf.Abs(oldShield - amount));
+        }
+        else
+        {
+            actualHealth = Mathf.Clamp(actualHealth - amount, 0, maxHealth);
+        }
+    }
+
+    public void HealLife(int amount)
+    {
+        if (amount < 1) return;
+        actualHealth = Mathf.Clamp(actualHealth - amount, 0, maxHealth);
+    }
+
+    public void HealArmor(int amount)
+    {
+        if (amount < 1) return;
+        actualShield = Mathf.Clamp(actualShield - amount, 0, maxShield);
+    }
+}
