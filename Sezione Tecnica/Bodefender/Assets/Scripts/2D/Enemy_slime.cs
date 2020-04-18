030ee6dd089c4b16f844b350929455b439bb8ff4
Andrea Belloni <abelloni2022@gmail.com>
2020-04-18 16:53:37 +0200

    Slime AI

---
+++ Sezione Tecnica/Bodefender/Assets/Scripts/2D/Enemy_slime.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_slime : MonoBehaviour
+{
+    public float speed;
+    public float time = 3;
+    float timeleft;
+    public bool Moveright;
+    void Update()
+    {
+        timeleft = timeleft - Time.deltaTime;
+        if(timeleft <= 0)
+        {
+            timeleft = time;
+            Moveright = !Moveright;
+        }
+        if (Moveright)
+        {
+            transform.Translate(1 * Time.deltaTime * speed,0,0);
+        }
+        else
+        {
+            transform.Translate(-1 * Time.deltaTime * speed, 0, 0);
+        }
+        
+    }
+
+    private void Start()
+    {
+        timeleft = time;
+    }
+
+    //In caso di scalini
+     private void OnTriggerEnter2D(Collider2D Oggetto)
+     {
+         //if(Oggetto.gameObject.CompareTag("IWall"))
+         if (Oggetto.GetComponent<Player>())
+         {
+            Moveright = !Moveright;
+         }
+     }
+     
+}
