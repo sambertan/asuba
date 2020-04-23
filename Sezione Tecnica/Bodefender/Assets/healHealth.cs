721c3887b4e189952cda5eb6cafb135ed33fb2d2
cannibutthenottakenone <48643174+cannibutthenottakenone@users.noreply.github.com>
2020-04-23 22:57:20 +0200

    healer per vita e scudo
    creazione di due nuovi oggetti prefab per la cura di vita e scudo

---
+++ Sezione Tecnica/Bodefender/Assets/healHealth.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class healHealth : MonoBehaviour
+{
+    public int healing;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Player player = collision.GetComponent<Player>();
+        if (player != null)
+        {
+            player.HealLife(healing);
+            Debug.Log("curato");
+        }
+    }
+}
---
+++ Sezione Tecnica/Bodefender/Assets/healShield.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class healShield : MonoBehaviour
+{
+    public int healing;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Player player = collision.GetComponent<Player>();
+        if (player != null)
+        {
+            player.HealArmor(healing);
+            Debug.Log("scudato");
+        }
+    }
+
+}
