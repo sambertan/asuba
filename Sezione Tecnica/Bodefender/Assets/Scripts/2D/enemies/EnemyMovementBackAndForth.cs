d00246b2faea92e58d1fa6f1f100c7aa53acdfe4
cannibutthenottakenone <48643174+cannibutthenottakenone@users.noreply.github.com>
2020-04-24 22:53:34 +0200

    Torretta
    la torretta ruota e detecta la distanza dal nemico sparando (per finta) quando si avvicina troppo.

---
+++ Sezione Tecnica/Bodefender/Assets/Scripts/2D/enemies/EnemyMovementBackAndForth.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovementBackAndForth : Enemy
+{
+    public float speed;
+    public float time;
+    float timeleft;
+    public bool moveRight;
+    public int dannoContatto;
+
+    SpriteRenderer sprite;
+
+
+    void Update()
+    {
+        timeleft = timeleft - Time.deltaTime;
+        if(timeleft <= 0)
+        {
+            timeleft = time;
+            moveRight = !moveRight;
+            sprite.flipX = !sprite.flipX;
+        }
+        if (moveRight)
+        {
+            transform.Translate(1 * Time.deltaTime * speed,0,0);
+        }
+        else
+        {
+            transform.Translate(-1 * Time.deltaTime * speed, 0, 0);
+        }
+        
+    }
+
+    private void Start()
+    {
+        timeleft = time;
+        sprite = GetComponent<SpriteRenderer>();
+        base.Start();
+    }
+
+    private void OnCollisionEnter2D(Collision2D Oggetto)
+    {
+        Player Giocatore = Oggetto.collider.GetComponent<Player>();
+        Terrain terreno = Oggetto.collider.GetComponent<Terrain>();
+        if (terreno != null)
+        {
+            return;
+        }
+        if (Giocatore != null)
+        {
+            Giocatore.Damage(dannoContatto);
+        }
+        this.timeleft = time - timeleft;
+        moveRight = !moveRight;
+        sprite.flipX = !sprite.flipX;
+    }
+
+}
---
+++ Sezione Tecnica/Bodefender/Assets/Scripts/2D/enemies/Torretta.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Torretta : Enemy
+{
+    public GameObject player;
+    Vector2 playerPosition;
+
+    public float radiusDetect=10;
+
+    Rigidbody2D rb;
+    Vector2 defaultPosition;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        defaultPosition = rb.position;
+
+        base.Start();
+    }
+
+    
+    void Update()
+    {
+        AngleSet();
+
+        rb.position = defaultPosition;
+
+        
+    }
+
+    void AngleSet()
+    {
+        playerPosition = player.transform.position;
+        Vector2 difference = playerPosition - rb.position;
+        rb.rotation = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg + 180f;
+        float distance = Mathf.Sqrt(Mathf.Pow(difference.x, 2) + Mathf.Pow(difference.y, 2));
+        if (distance < radiusDetect)
+            Debug.Log("pew!");
+
+        
+    }
+
+    void Shoot()
+    {
+
+    }
+}
