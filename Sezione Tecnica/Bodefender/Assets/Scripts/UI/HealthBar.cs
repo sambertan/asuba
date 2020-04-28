4d6317ada6ee026ccda18ecf203d081a62159b94
cannibutthenottakenone <48643174+cannibutthenottakenone@users.noreply.github.com>
2020-04-28 21:47:36 +0200

    Barra della vita
    Implemantazione di un canvas che carica un immagine diversa a seconda della vita.
    Manca l'implementazione della stessa cosa per scudo e priettili.
    Fatto anche un lavoro di unificazione degli sprite della vita in un set

---
+++ Sezione Tecnica/Bodefender/Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Sprite[] status= new Sprite[10];
+    Image SpriteSImage;
+    Player player;
+
+    private void Start()
+    {
+        SpriteSImage = GetComponent<Image>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
+
+    private void FixedUpdate()
+    {
+        SpriteSImage.sprite=status[player.ActualHealth-1];
+    }
+
+
+}
