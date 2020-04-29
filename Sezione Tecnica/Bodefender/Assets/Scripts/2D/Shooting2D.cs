e97ae886decf4b5c73a3a1639165b34e4f3f6248
cannibutthenottakenone <48643174+cannibutthenottakenone@users.noreply.github.com>
2020-04-29 22:39:08 +0200

    Meccanica di ricarica, nuovi sprites per l'HUD @federicobenti
    Co-Authored-By: federicobenti <federicobenti@users.noreply.github.com>

--- Sezione Tecnica/Bodefender/Assets/Scripts/2D/Shooting2D.cs
+++ Sezione Tecnica/Bodefender/Assets/Scripts/2D/Shooting2D.cs
@@ -2,20 +2,58 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Weapon : MonoBehaviour
+public class Shooting2D : MonoBehaviour
 {
     public Transform firePoint;
     public GameObject bulletPrefab;
+
+    public float reloadTime=3;
+    public float reloadLeft;
+    bool alreadyReloaded=true;
+
+    int maxbullet = 10;
+    public int bulletsleft;
+
+        
+    private void Start()
+    {
+        bulletsleft = maxbullet;
+    }
+
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (reloadLeft > 0)
         {
-            Shoot();
+            reloadLeft -= Time.deltaTime;
         }
+        else if(!alreadyReloaded)
+        {
+            alreadyReloaded = true;
+            bulletsleft = maxbullet;
+        }
+        else
+        {
+            if (Input.GetButtonDown("Fire1") && bulletsleft > 0)
+            {
+                Shoot();
+                bulletsleft--;
+            }
+            if (Input.GetKeyDown(KeyCode.R) && bulletsleft < maxbullet)
+            {
+                reload();
+            }
+        }
+         
     }
 
     void Shoot()
     {
         Instantiate(bulletPrefab, firePoint.position , firePoint.rotation);
+    }
+
+    void reload()
+    {
+        reloadLeft = reloadTime;
+        alreadyReloaded = false;
     }
 }
