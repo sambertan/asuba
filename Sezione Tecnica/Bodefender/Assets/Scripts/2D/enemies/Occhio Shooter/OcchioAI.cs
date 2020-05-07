bcdb5490fd5820d88a2226e71c6ea574dca69f38
cannibutthenottakenone <48643174+cannibutthenottakenone@users.noreply.github.com>
2020-05-07 14:24:36 +0200

    Eye movement seem to work well

--- Sezione Tecnica/Bodefender/Assets/Scripts/2D/enemies/Occhio Shooter/OcchioAI.cs
+++ Sezione Tecnica/Bodefender/Assets/Scripts/2D/enemies/Occhio Shooter/OcchioAI.cs
@@ -1,40 +1,86 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OcchioAI : MonoBehaviour
+public class OcchioAI : Enemy
 {
+    //to track the player a interact with it
     Transform target;
-    public float speed=10;
+    public float speed = 10;
     public float viewRange = 15;
     public float shootingRange = 8;
     float distance;
+    float distancex;
+    float distancey;
 
+    //to move the sprite
     SpriteRenderer sprite;
+    bool direction;
+
+    //to jump
+    Rigidbody2D rb;
+    public float jumpRechargeTime;
+    float jumpTimeLeft;
 
 
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         sprite = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
 
     void Update()
     {
-        distance = Mathf.Sqrt(Mathf.Pow(transform.position.x - target.position.x, 2) + Mathf.Pow(transform.position.y - target.position.y, 2));
+        distancex = transform.position.x - target.position.x;
+        direction = Mathf.Sign(distancex) == 1;
+        distancex = Mathf.Abs(distancex);
+
+        distancey = target.position.y - transform.position.y;
+
+        distance = Mathf.Sqrt(Mathf.Pow(distancex, 2) + Mathf.Pow(distancey, 2));
+
+        sprite.flipX = !direction;
+
         if (distance <= viewRange && distance >= shootingRange)
-            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
-        else if (distance <= shootingRange&& (target.position.y-transform.position.y) <2.7f)
-            Shoot();
+        {
+            transform.position = Vector2.MoveTowards(transform.position, new Vector2(target.position.x, transform.position.y), speed * Time.deltaTime);
+            if (distancey < 5.4f && distancey > 1.6f)
+                Jump();
+        }
+
+        else if (distance <= shootingRange)
+        {
+            if (distancey < 5.4f && distancey > 1.6f)
+                Jump();
+
+            distancey = target.position.y - transform.position.y;
+
+
+            if (distancey < 1.6f)
+                Shoot();
+        }
 
 
 
 
+        jumpTimeLeft -= Time.deltaTime;
+    }
+
+    private void Jump()
+    {
+        if (jumpTimeLeft <= 0)
+        {
+            jumpTimeLeft = jumpRechargeTime;
+            rb.velocity += new Vector2(0, 8);
+            Debug.Log("jumpato");
+        }
     }
 
     void Shoot()
     {
-        Debug.Log("Pew Pew Pew");
+        //Debug.Log("Pew Pew Pew");
     }
 }
